b80a24ceffb266958c53d5009d4ef1b323616358
udayeni.anumala <udayeni.anumala@BLRKEC350616L.mshome.net>
2020-09-10 19:37:53 +0530

    docker thread

--- src/test/java/docker/ChromeTest.java
+++ src/test/java/docker/ChromeTest.java
@@ -9,13 +9,16 @@ import org.testng.annotations.Test;
 
 public class ChromeTest {
 	@Test
-	public static void grid1() throws MalformedURLException{
+	public static void grid1() throws MalformedURLException, InterruptedException{
 DesiredCapabilities dc=DesiredCapabilities.chrome();
+		
 		
 		URL url=new URL("http://localhost:4545/wd/hub");
 		
 		RemoteWebDriver driver=new RemoteWebDriver(url,dc);
+		Thread.sleep(5000);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
 		System.out.println("title of page"+driver.getTitle());
 		driver.quit();
 		
--- src/test/java/docker/FirefoxTest.java
+++ src/test/java/docker/FirefoxTest.java
@@ -10,13 +10,15 @@ import org.testng.annotations.Test;
 public class FirefoxTest {
 
 	@Test
-public static void grid2() throws MalformedURLException{
+public static void grid2() throws MalformedURLException, InterruptedException{
 		DesiredCapabilities dc=DesiredCapabilities.firefox();
 				
 		URL url=new URL("http://localhost:4545/wd/hub");
 				
 		RemoteWebDriver driver=new RemoteWebDriver(url,dc);
+		Thread.sleep(5000);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
 		System.out.println("title of page"+driver.getTitle());
 		driver.quit();
 				
