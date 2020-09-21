b8d0ba178069f69c1c594208e5da91b1ef439130
udayeni.anumala <udayeni.anumala@BLRKEC350616L.mshome.net>
2020-09-21 18:50:22 +0530

    bat file

--- buildjenkins.bat
+++ buildjenkins.bat
@@ -1,4 +1,2 @@
 call mvn clean test -DsuiteXmlFile=testng.xml
 call mvn clean test -DsuiteXmlFile=testngfirefox.xml
-call docker build -f DockerfileChrome . -t=image1
-call docker build -f DockerfileFirefox . -t=image2
